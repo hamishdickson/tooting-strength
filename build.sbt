3f6ebb16a7ffaa9435a363faa5ed8999baea5cb3
hamishdickson <hamish.dickson@gmail.com>
2016-04-23 12:13:05 +0100

    first commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "tooting-strength"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.0"
---
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,12 @@
+package tutorial.webapp
+
+import scala.scalajs.js.JSApp
+
+import org.scalajs.dom
+import dom.document
+
+object TutorialApp extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
